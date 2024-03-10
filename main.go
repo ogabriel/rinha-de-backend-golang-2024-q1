1d7041d5c3cf4d9a9ad5589a34eae0c2feced650
ogabriel <gabrieloliver8991@gmail.com>
2024-03-10 12:09:36 -0300

    add base main.go

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	connString := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable&pool_min_conns=%s&pool_max_conns=%[6]s",
+		getEnv("DATABASE_USER"),
+		getEnv("DATABASE_PASS"),
+		getEnv("DATABASE_HOST"),
+		getEnv("DATABASE_PORT"),
+		getEnv("DATABASE_NAME"),
+		getEnv("DATABASE_POOL"),
+	)
+
+	config, err := pgxpool.ParseConfig(connString)
+
+	if err != nil {
+		panic(err)
+	}
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(context.Background()); err != nil {
+		panic(err)
+	}
+
+	router := gin.New()
+	router.Use(gin.Recovery())
+
+	router.POST("/clientes/:id/transacoes", transacoes(pool))
+	router.GET("/clientes/:id/extrato", extrato(pool))
+
+	router.Run("127.0.0.1:" + getEnv("PORT"))
+}
+
+func getEnv(envName string) string {
+	env, ok := os.LookupEnv(envName)
+
+	if !ok {
+		message := fmt.Sprintf("env not declared $%s", envName)
+		panic(message)
+	}
+
+	return env
+}
+
+type Cliente struct {
+	ID     int `json:"id"`
+	Saldo  int `json:"saldo"`
+	Limite int `json:"limite"`
+}
+
+type Transacao struct {
+	Valor       int    `json:"valor"`
+	Tipo        string `json:"tipo"`
+	Descricao   string `json:"descricao"`
+	RealizadaEm string `json:"realizada_em"`
+}
+
+func transacoes(pool *pgxpool.Pool) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+	}
+}
+
+func extrato(pool *pgxpool.Pool) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+	}
+}
